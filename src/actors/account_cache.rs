7b124e803a6c8217768945dbe40b3df5552ae216
Mike Hostetler <mike.hostetler@siriusxm.com>
2024-02-02 11:48:31 -0600

    Allow ability to specify BATCH_INTERVAL from env

--- src/actors/account_cache.rs
+++ src/actors/account_cache.rs
@@ -1,9 +1,15 @@
+use crate::{
+    Account, AccountCacheMessage, Address, RpcMessage, RpcResponseError, TransactionResponse,
+};
 use async_trait::async_trait;
 use futures::stream::FuturesUnordered;
-use ractor::{concurrency::OneshotReceiver, Actor, ActorRef, ActorProcessingErr};
+use ractor::{concurrency::OneshotReceiver, Actor, ActorProcessingErr, ActorRef};
+use std::{
+    collections::HashMap,
+    fmt::Display,
+    time::{Duration, Instant},
+};
 use thiserror::Error;
-use std::{collections::HashMap, fmt::Display, time::{Duration, Instant}};
-use crate::{Address, Account, AccountCacheMessage, TransactionResponse, RpcMessage, RpcResponseError};
 
 #[derive(Debug, Clone)]
 pub struct AccountCacheActor;
@@ -29,33 +35,33 @@ pub struct AccountCache {
     cache: HashMap<Address, Account>,
     receivers: FuturesUnordered<OneshotReceiver<Address>>,
     batch_interval: Duration,
-    last_batch: Option<Instant>
+    last_batch: Option<Instant>,
 }
 
 impl AccountCache {
-    pub fn new(
-    ) -> Self {
+    pub fn new() -> Self {
+        let batch_interval_secs = std::env::var("BATCH_INTERVAL")
+            .unwrap_or_else(|_| "180".to_string())
+            .parse::<u64>()
+            .unwrap_or(180);
         Self {
             cache: HashMap::new(),
             receivers: FuturesUnordered::new(),
-            batch_interval: Duration::from_secs(180),
-            last_batch: None 
+            batch_interval: Duration::from_secs(batch_interval_secs),
+            last_batch: None,
         }
     }
 
-    pub(crate) fn get(
-        &self,
-        address: &Address
-    ) -> Option<&Account> {
+    pub(crate) fn get(&self, address: &Address) -> Option<&Account> {
         if let Some(account) = self.cache.get(address) {
-            return Some(account)
+            return Some(account);
         }
-        return None
+        return None;
     }
 
     pub(crate) fn remove(
         &mut self,
-        address: &Address
+        address: &Address,
     ) -> Result<(), Box<dyn std::error::Error + Send>> {
         self.cache.remove(address);
         Ok(())
@@ -67,48 +73,64 @@ impl AccountCache {
     ) -> Result<(), Box<dyn std::error::Error + Send>> {
         if let Some(a) = self.cache.get_mut(&account.owner_address()) {
             *a = account;
-            return Ok(())
+            return Ok(());
         }
 
-        return Err(Box::new(AccountCacheError) as Box<dyn std::error::Error + Send>)
+        return Err(Box::new(AccountCacheError) as Box<dyn std::error::Error + Send>);
     }
 
     pub(crate) fn handle_cache_write(
         &mut self,
-        account: Account
+        account: Account,
     ) -> Result<(), Box<dyn std::error::Error + Send>> {
         match account.account_type() {
             crate::AccountType::User => {
-                let address = account.owner_address(); 
+                let address = account.owner_address();
                 if let Some(entry) = self.cache.get_mut(&address) {
                     log::info!("Found account: 0x{:x} in cache, updating...", &address);
                     *entry = account;
                 } else {
-                    log::info!("Did not find account: 0x{:x} in cache, inserting...", &address);
+                    log::info!(
+                        "Did not find account: 0x{:x} in cache, inserting...",
+                        &address
+                    );
                     self.cache.insert(address, account);
-                    log::info!("Inserted account: 0x{:x} in cache, cache.len(): {}", &address, self.cache.len());
+                    log::info!(
+                        "Inserted account: 0x{:x} in cache, cache.len(): {}",
+                        &address,
+                        self.cache.len()
+                    );
                 }
             }
             crate::AccountType::Program(program_address) => {
                 if let Some(_entry) = self.cache.get_mut(&program_address) {
-                    log::info!("Found program_account: 0x{:x} in cache, updating...", &program_address);
+                    log::info!(
+                        "Found program_account: 0x{:x} in cache, updating...",
+                        &program_address
+                    );
                 } else {
-                    log::info!("Did not find account: 0x{:x} in cache, inserting...", &program_address);
+                    log::info!(
+                        "Did not find account: 0x{:x} in cache, inserting...",
+                        &program_address
+                    );
                     self.cache.insert(program_address, account);
-                    log::info!("Inserted account: 0x{:x} in cache, cache.len(): {}", &program_address, self.cache.len());
+                    log::info!(
+                        "Inserted account: 0x{:x} in cache, cache.len(): {}",
+                        &program_address,
+                        self.cache.len()
+                    );
                 }
             }
         }
 
         self.check_build_batch()?;
-        
+
         Ok(())
     }
 
     fn check_build_batch(&mut self) -> Result<(), Box<dyn std::error::Error + Send>> {
-        let bytes = serde_json::to_vec(&self.cache).map_err(|e| {
-            Box::new(e) as Box<dyn std::error::Error + Send>
-        })?;
+        let bytes = serde_json::to_vec(&self.cache)
+            .map_err(|e| Box::new(e) as Box<dyn std::error::Error + Send>)?;
         if base64::encode(&bytes).len() >= crate::MAX_BATCH_SIZE {
             self.build_batch()?;
         } else if let Some(instant) = self.last_batch {
@@ -130,22 +152,22 @@ impl AccountCache {
 
 impl AccountCacheActor {
     pub fn new() -> Self {
-        Self 
+        Self
     }
 }
 
 #[async_trait]
 impl Actor for AccountCacheActor {
     type Msg = AccountCacheMessage;
-    type State = AccountCache; 
+    type State = AccountCache;
     type Arguments = ();
-    
+
     async fn pre_start(
         &self,
         _myself: ActorRef<Self::Msg>,
         _: (),
     ) -> Result<Self::State, ActorProcessingErr> {
-        Ok(AccountCache::new()) 
+        Ok(AccountCache::new())
     }
 
     async fn handle(
@@ -156,12 +178,18 @@ impl Actor for AccountCacheActor {
     ) -> Result<(), ActorProcessingErr> {
         match message {
             AccountCacheMessage::Write { account } => {
-                log::info!("Received account cache write request: 0x{:x}", &account.owner_address());
+                log::info!(
+                    "Received account cache write request: 0x{:x}",
+                    &account.owner_address()
+                );
                 let _ = state.handle_cache_write(account.clone());
                 log::info!("Account: {:?}", &account);
             }
             AccountCacheMessage::Read { address, tx } => {
-                log::info!("Recieved account cache read request for account: {:?}", &address); 
+                log::info!(
+                    "Recieved account cache read request for account: {:?}",
+                    &address
+                );
                 let account = state.get(&address);
                 log::info!("acquired account option: {:?}", &account);
                 let _ = tx.send(account.cloned());
@@ -176,19 +204,18 @@ impl Actor for AccountCacheActor {
             }
             AccountCacheMessage::TryGetAccount { address, reply } => {
                 if let Some(account) = state.get(&address) {
-                    let _ = reply.send(
-                        RpcMessage::Response { 
-                            response: Ok(TransactionResponse::GetAccountResponse(account.clone())),
-                            reply: None 
-                        }
-                    );
+                    let _ = reply.send(RpcMessage::Response {
+                        response: Ok(TransactionResponse::GetAccountResponse(account.clone())),
+                        reply: None,
+                    });
                 } else {
                     // Pass along to EO
                     // EO passes along to DA if Account Blob discovered
                     let response = Err(RpcResponseError);
-                    let _ = reply.send(
-                        RpcMessage::Response { response, reply: None }
-                    );
+                    let _ = reply.send(RpcMessage::Response {
+                        response,
+                        reply: None,
+                    });
                 }
             }
         }
