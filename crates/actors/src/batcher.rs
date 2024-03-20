3db36143e1cf5ad071a82e56fad9a7ccfb1acb92
Andrew Smith <asmith@vrrb.io>
2024-03-20 02:11:39 -0400

    fix bug in token update application

--- crates/actors/src/batcher.rs
+++ crates/actors/src/batcher.rs
@@ -961,7 +961,7 @@ impl Batcher {
                 }
             }
             AddressOrNamespace::Address(address) => {
-                if let Some(mut account) = batch_buffer.get_mut(&transaction.to()) {
+                if let Some(mut account) = batch_buffer.get_mut(&transaction.from()) {
                     if let Some(program_account) = get_account(program_id.clone()).await {
                         account.apply_token_update(
                             &program_id, token_update.updates(), &program_account
@@ -1134,6 +1134,7 @@ impl Batcher {
     ) -> Result<Account, BatcherError> {
         match update {
             TokenOrProgramUpdate::TokenUpdate(token_update) => {
+                log::warn!("received token update: {:?}", token_update);
                 self.apply_token_update(transaction, &token_update, batch_buffer).await
             }
             TokenOrProgramUpdate::ProgramUpdate(program_update) => {
