3020f006d21292e9c8b24c07923649bb89946c2e
Mike Hostetler <mike.hostetler@gmail.com>
2024-03-20 16:55:30 -0700

    Silently fail if no git hash

--- crates/node/build.rs
+++ crates/node/build.rs
@@ -1,16 +1,17 @@
 use std::process::Command;
 fn main() {
-    let output = Command::new("git")
+    let git_hash_output = Command::new("git")
         .args(&["rev-parse", "HEAD"])
-        .output()
-        .unwrap();
-    let mut git_hash = String::from_utf8(output.stdout).unwrap();
-    git_hash.pop(); // trailing \n
-    let output = Command::new("git")
+        .output();
+    let git_branch_output = Command::new("git")
         .args(&["branch", "--show-current"])
-        .output()
-        .unwrap();
-    let mut git_branch = String::from_utf8(output.stdout).unwrap();
-    git_branch.pop(); // remove trailing \n
-    println!("cargo:rustc-env=GIT_REV={}:{}", git_branch, git_hash);
+        .output();
+
+    if let (Ok(hash_output), Ok(branch_output)) = (git_hash_output, git_branch_output) {
+        if let (Ok(mut git_hash), Ok(mut git_branch)) = (String::from_utf8(hash_output.stdout), String::from_utf8(branch_output.stdout)) {
+            git_hash.pop(); // trailing \n
+            git_branch.pop(); // remove trailing \n
+            println!("cargo:rustc-env=GIT_REV={}:{}", git_branch, git_hash);
+        }
+    }
 }
