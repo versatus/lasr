63a715be4d1f5f4b09e6ccb48858186c5f111844
Andrew O'Brien <ohbandrew@gmail.com>
2024-06-21 13:50:34 -0500

    wip

--- crates/eo_listener/src/lib.rs
+++ crates/eo_listener/src/lib.rs
@@ -30,8 +30,8 @@ macro_rules! log_handler {
     };
 }
 pub fn get_abi() -> Result<web3::ethabi::Contract, EoServerError> {
-    web3::ethabi::Contract::load(&include_bytes!("eo_contract_abi.json")[..])
-        .map_err(|e| EoServerError::Other(e.to_string()))
+    let json = &include_bytes!("eo_contract_abi.json")[..];
+    web3::ethabi::Contract::load(json).map_err(|e| EoServerError::Other(e.to_string()))
 }
 
 pub fn get_blob_index_settled_topic() -> Option<Vec<H256>> {
